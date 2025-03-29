e6c12f56aa5741999746d92ab519fd352c79055f
hemanna <parvatihanaval137@gmail.com>
2025-03-29 13:21:57 +0530

    user api

---
+++ src/main/java/com/sbd/common/Jsonb/UserCredentialRequest.java
@@ -0,0 +1,11 @@
+package com.sbd.common.Jsonb;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserCredentialRequest {
+    public String username;
+    private String password;
+}
